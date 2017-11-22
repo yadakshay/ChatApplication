4afa4e857f550a2ac6b073c67576965068f9e822
Akshay <yadakshay@gmail.com>
2017-11-22 19:09:50 +0530

    commit with chat bubbles and reponse

--- app/src/main/java/com/example/user/chatapplication/ChatActivity.java
+++ app/src/main/java/com/example/user/chatapplication/ChatActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,11 +17,13 @@ import android.widget.Toast;
 import com.example.user.chatapplication.Database.chatDatabaseContract;
 import com.example.user.chatapplication.Database.chatDbHelper;
 
+import static com.example.user.chatapplication.Database.chatDatabaseContract.chatEntry.TABLE_NAME;
+
 public class ChatActivity extends AppCompatActivity {
     private EditText enteredText;
     private RecyclerView chat;
-    private String SENT_KEY = "sent";
-    private String RECEIVE_KEY = "received";
+    public static String SENT_KEY = "sent";
+    public static String RECEIVE_KEY = "received";
     private chatDbHelper mDbHelper;
     private SQLiteDatabase db;
     private customAdapter mAdapter;
@@ -35,15 +39,16 @@ public class ChatActivity extends AppCompatActivity {
         db = mDbHelper.getWritableDatabase();
 
         Cursor cursor = getAllMessages();
-        Toast.makeText(this, cursor.toString(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, cursor.toString(), Toast.LENGTH_SHORT).show();
         chat.setLayoutManager(new LinearLayoutManager(this));
         mAdapter = new customAdapter(this, cursor);
         chat.setAdapter(mAdapter);
+        chat.scrollToPosition(cursor.getCount() - 1);
     }
 
     private Cursor getAllMessages() {
         return db.query(
-                chatDatabaseContract.chatEntry.TABLE_NAME,
+                TABLE_NAME,
                 null,
                 null,
                 null,
@@ -62,13 +67,41 @@ public class ChatActivity extends AppCompatActivity {
         cv.put(chatDatabaseContract.chatEntry.COLUMN_CHAT_MESSAGE, message);
         cv.put(chatDatabaseContract.chatEntry.COLUMN_SENT_RECEIVED, SENT_KEY);
 
-        long insertId = db.insert(chatDatabaseContract.chatEntry.TABLE_NAME, null, cv);
+        long insertId = db.insert(TABLE_NAME, null, cv);
         if(insertId > 0){
-            Toast.makeText(this, "inserted!", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, "inserted!", Toast.LENGTH_SHORT).show();
+            sendRandomReply();
+            Cursor c = getAllMessages();
             enteredText.setText(null);
-            mAdapter.swapCursor(getAllMessages());
+            mAdapter.swapCursor(c);
+            chat.scrollToPosition(c.getCount() - 1);
         }else{
             Toast.makeText(this, "failed!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void sendRandomReply() {
+        String response = "Random Response";
+        ContentValues cvResponse = new ContentValues();
+        cvResponse.put(chatDatabaseContract.chatEntry.COLUMN_CHAT_MESSAGE, response);
+        cvResponse.put(chatDatabaseContract.chatEntry.COLUMN_SENT_RECEIVED, RECEIVE_KEY);
+        long insertId = db.insert(TABLE_NAME, null, cvResponse);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.clearChat){
+            db.delete(TABLE_NAME, null, null);
+            mAdapter.swapCursor(getAllMessages());
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/user/chatapplication/customAdapter.java
+++ app/src/main/java/com/example/user/chatapplication/customAdapter.java
@@ -3,12 +3,17 @@ package com.example.user.chatapplication;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.user.chatapplication.Database.chatDatabaseContract;
+
+import static com.example.user.chatapplication.ChatActivity.SENT_KEY;
 
 /**
  * Created by Akshay on 22-11-2017.
@@ -36,7 +41,32 @@ public class customAdapter extends RecyclerView.Adapter<customAdapter.customAdap
             return; // return if null
 
         String text = mCursor.getString(mCursor.getColumnIndex(chatDatabaseContract.chatEntry.COLUMN_CHAT_MESSAGE));
+        String messageType = mCursor.getString(mCursor.getColumnIndex(chatDatabaseContract.chatEntry.COLUMN_SENT_RECEIVED));
         long id = mCursor.getLong(mCursor.getColumnIndex(chatDatabaseContract.chatEntry._ID));
+
+        if(messageType.matches(SENT_KEY)){
+            holder.relativeLayout.setBackgroundResource(R.drawable.outgoing_message);
+            RelativeLayout.LayoutParams lp =
+                    (RelativeLayout.LayoutParams) holder.messageTextView.getLayoutParams();
+            lp.addRule(RelativeLayout.ALIGN_PARENT_LEFT, 0);
+            holder.messageTextView.setLayoutParams(lp);
+
+            LinearLayout.LayoutParams layParam =
+                    (LinearLayout.LayoutParams) holder.relativeLayout.getLayoutParams();
+            layParam.gravity = Gravity.RIGHT;
+            holder.relativeLayout.setLayoutParams(layParam);
+        }else{
+            holder.relativeLayout.setBackgroundResource(R.drawable.incomming_message);;
+            RelativeLayout.LayoutParams lp =
+                    (RelativeLayout.LayoutParams) holder.messageTextView.getLayoutParams();
+            lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, 0);
+            holder.messageTextView.setLayoutParams(lp);
+
+            LinearLayout.LayoutParams layParam =
+                    (LinearLayout.LayoutParams) holder.relativeLayout.getLayoutParams();
+            layParam.gravity = Gravity.LEFT;
+            holder.relativeLayout.setLayoutParams(layParam);
+        }
         holder.messageTextView.setText(text);
         holder.itemView.setTag(id);
     }
@@ -48,9 +78,11 @@ public class customAdapter extends RecyclerView.Adapter<customAdapter.customAdap
 
     public class customAdapterViewHolder extends RecyclerView.ViewHolder{
         TextView messageTextView;
+        RelativeLayout relativeLayout;
         public customAdapterViewHolder(View itemView) {
             super(itemView);
             messageTextView = (TextView) itemView.findViewById(R.id.messageText);
+            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.layoutContainingBackground);
         }
     }
 
